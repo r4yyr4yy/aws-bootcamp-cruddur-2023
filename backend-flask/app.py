cd9e1c04772f85634ee0a5e044637c4fb4232f6a
r4yyr4yy <rayisraelokudzeto@gmail.com>
2023-03-16 00:49:05 +0000

    SigninPage created

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -4,7 +4,6 @@ from flask_cors import CORS, cross_origin
 import os
 
 from services.home_activities import *
-from services.notifications_activities import *
 from services.user_activities import *
 from services.create_activity import *
 from services.create_reply import *
@@ -14,49 +13,7 @@ from services.messages import *
 from services.create_message import *
 from services.show_activity import *
 
-# honeycomb --------------
-from opentelemetry import trace
-from opentelemetry.instrumentation.flask import FlaskInstrumentor
-from opentelemetry.instrumentation.requests import RequestsInstrumentor
-from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
-from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchSpanProcessor
-from opentelemetry.sdk.trace.export import ConsolepanExporter, SimpleSpanProcessor
-
-# AWS XRay --------------
-from aws_xray_sdk.core import xray_recorder
-from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
-
-# honeycomb --------------
-# Initialize tracing and an exporter that can send data to Honeycomb
-provider = TracerProvider()
-processor = BatchSpanProcessor(OTLPSpanExporter())
-provider.add_span_processor(processor)
-
-trace.set_tracer_provider(provider)
-tracer = trace.get_tracer(__name__)
-
-# Show this the logs within the backend-flask app
-
-simple_processor = SimpleSpanProcessor(ConsoleSpanExporter())
-provider.add_span_processor(simple_processor)
-
-trace.set_tracer_provider(provider)
-tracer = trace.get_tracer(__name__)
-
 app = Flask(__name__)
-
-# AWS XRay --------------
-xray_url = os.getenv("AWS_XRAY_URL")
-xray_recorder.configure(service='Cruddur', dynamic_naming=xray_url)
-XRayMiddleware(app, xray_recorder)
-
-# honeycomb --------------
-# Initialize automatic instrumentation with Flask
-
-FlaskInstrumentor().instrument_app(app)
-RequestsInstrumentor().instrument()
-
 frontend = os.getenv('FRONTEND_URL')
 backend = os.getenv('BACKEND_URL')
 origins = [frontend, backend]
@@ -106,11 +63,6 @@ def data_create_message():
 @app.route("/api/activities/home", methods=['GET'])
 def data_home():
   data = HomeActivities.run()
-  return data, 200
-
-@app.route("/api/activities/notifications", methods=['GET'])
-def data_notifications():
-  data = NotificationsActivities.run()
   return data, 200
 
 @app.route("/api/activities/@<string:handle>", methods=['GET'])
